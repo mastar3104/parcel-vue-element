8cd3d9650d32ba366888056dc36822987181d9e6
mastar3104 <hcs.sasajima@gmail.com>
2017-12-20 21:50:44 +0900

    vue parcel test app

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ja'
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App.vue'
+
+Vue.use(ElementUI, {locale})
+
+new Vue(App).$mount('#app')
